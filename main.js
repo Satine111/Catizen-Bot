fb0e4285f5e8a15e847cb35636d80a504d686c14
Satine <satines111@gmail.com>
2024-08-15 01:41:43 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 # Catizen Bot
 
 ## Features
-- **Auto Merge**: Automatically merge your cats.
-- **Auto Boost**: Boost your speed and efficiency.
-- **Auto Claim Airdrop Cat**: Automatically claim airdrops.
-- **Auto Claim Gold Boost**: Automatically claim gold boosts.
-- **Auto Claim Gold**: Automatically claim your gold.
-- **Auto Claim Offline Gold**: Automatically claim offline gold.
-- **Multi-Account**: Manage multiple accounts.
-- **Support RDP/VPS/Termux**: Compatible with various os platforms.
-- **Less Memory/CPU Usage**: Optimized for efficient memory usage.
+- **Auto Merge**
+- **Auto Boost**
+- **Auto Claim Airdrop Cat**
+- **Auto Claim Gold Boost**
+- **Auto Claim Gold**
+- **Auto Claim Offline Gold**
+- **Multi-Account**
+- **Support RDP/VPS/Termux**
+- **Less Memory/CPU Usage**
 - **Support Proxy**
-- **And many more features**: Numerous additional features to help you speed up Catizen!
 
 ## Prices
-- **1 Month**: 5 USDT
-- **Lifetime**: 10 USDT
+- **1 Month**: 4 USDT
+- **Lifetime**: 9 USDT
 
+Level 40+ under 9Days! Proof?Chat me
 ## Contact
 Reach me at [t.me/azamjawaa](https://t.me/azamjawaa) for more information.
