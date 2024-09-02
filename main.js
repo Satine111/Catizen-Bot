1bacd56a583e288226bd205d1fed2258c76b3e9a
Satine <satines111@gmail.com>
2024-09-02 15:21:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -16,11 +16,5 @@
 - **1 Month**: 5 USDT / Rp.70.000 (Unlimited Account)
 - **Lifetime**: 9 USDT / Rp.130.000 (Unlimited Account)
 
-## JASTIP/RENT Catizen Bot Run 24Hours
-Only need Query
-- **1Month**: 3 USDT / Rp.35.000 (Max 5 Acc)
-- **Lifetime**: 5 USDT / Rp.70.000 (Max 7 Acc)
-Level 70-100 in 1 Month!
-
 ## Contact
 Reach me at [t.me/azamjawaa](https://t.me/azamjawaa) for more information.
