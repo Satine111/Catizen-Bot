62dc7983e97ffe9c8ee605ac3dad7d2019970ebd
Satine <satines111@gmail.com>
2024-09-04 20:10:47 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 # Catizen Bot
 
+![image](https://github.com/user-attachments/assets/956dd03e-2e8e-4b19-a45e-fafccdbb16de)
+
+
 ## Features
 - **Auto Merge**
 - **Auto Boost**
@@ -13,8 +16,8 @@
 - **Support Proxy**
 
 ## Prices Script
-- **1 Month**: 5 USDT / Rp.70.000 (Unlimited Account)
-- **Lifetime**: 9 USDT / Rp.130.000 (Unlimited Account)
+- **1 Month**: 4 USDT / Rp.50.000 (Unlimited Account)
+- **Lifetime**: 7 USDT / Rp.70.000 (Unlimited Account)
 
 ## Contact
 Reach me at [t.me/azamjawaa](https://t.me/azamjawaa) for more information.
